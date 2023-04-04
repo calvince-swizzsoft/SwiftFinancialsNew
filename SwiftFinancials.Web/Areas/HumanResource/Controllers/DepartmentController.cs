ab749563bcf691878a7e25a6b996b197626c34f8
stamline <stamlinetechnologies@gmail.com>
2023-04-04 14:33:17 +0300

    Add area hr

---
+++ SwiftFinancials.Web/Areas/HumanResource/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwiftFinancials.Web.Areas.HumanResource.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: HumanResource/Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
