22e8cb7cfbc9471382662bfa9e4f4d61a7be108f
stamline <stamlinetechnologies@gmail.com>
2023-04-13 10:15:16 +0300

    Add global lookup

---
+++ SwiftFinancials.Web/Controllers/LookupController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SwiftFinancials.Web.Controllers
+{
+    public class LookupController : MasterController
+    {
+        // GET: Lookup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetCompaniesAsync()
+        {
+            var companiesDTOs = await _channelService.FindCompaniesAsync(GetServiceHeader());
+
+            return Json(companiesDTOs, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
