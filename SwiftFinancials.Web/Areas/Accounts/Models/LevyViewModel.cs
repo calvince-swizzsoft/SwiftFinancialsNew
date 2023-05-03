b1a7f96837f3293dc460dbc55abad1d1962c27aa
stamline <stamlinetechnologies@gmail.com>
2023-05-03 13:00:59 +0300

    levy++

---
+++ SwiftFinancials.Web/Areas/Accounts/Models/LevyViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Application.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SwiftFinancials.Web.Areas.Accounts.Models
+{
+    public class LevyViewModel : BindingModelBase<LevyViewModel>
+    {
+        public LevyViewModel()
+        {
+            AddAllAttributeValidators();
+        }
+
+        [DataMember]
+        [Display(Name = "Name")]
+        [Required]
+        public string LevyDescription { get; set; }
+
+        [DataMember]
+        [Display(Name = "Is Locked?")]
+        public bool LevyIsLocked { get; set; }
+
+        [DataMember]
+        [Display(Name = "Levy Split Name")]
+        [Required]
+        public string LevySplitDescription { get; set; }
+
+        [DataMember]
+        [Display(Name = "COA")]
+        public Guid LevySplitChartOfAccountId { get; set; }
+
+        [DataMember]
+        [Display(Name = "Percentage")]
+        public double LevySplitPercentage { get; set; }
+    }
+}
