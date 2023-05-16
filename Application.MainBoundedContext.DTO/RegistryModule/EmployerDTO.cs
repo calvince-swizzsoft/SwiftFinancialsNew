f2723e75e9c16f5682014826729a2e3f545ac87a
Phinbett <102291279+Phinbett@users.noreply.github.com>
2023-05-16 11:47:41 +0300

    divisionEmployer

--- Application.MainBoundedContext.DTO/RegistryModule/EmployerDTO.cs
+++ Application.MainBoundedContext.DTO/RegistryModule/EmployerDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Application.MainBoundedContext.DTO.RegistryModule
@@ -46,5 +47,7 @@ namespace Application.MainBoundedContext.DTO.RegistryModule
 
         [Display(Name = "Created Date")]
         public DateTime CreatedDate { get; set; }
+
+        public IList<DivisionDTO> Divisions { get; set; }
     }
 }
