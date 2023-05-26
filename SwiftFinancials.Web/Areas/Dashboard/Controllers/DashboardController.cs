ea992e066e49b2a702873cf1a35984ed5489f357
stamline <stamlinetechnologies@gmail.com>
2023-05-26 16:45:09 +0300

    Update  dashboard metrics

--- SwiftFinancials.Web/Areas/Dashboard/Controllers/DashboardController.cs
+++ SwiftFinancials.Web/Areas/Dashboard/Controllers/DashboardController.cs
@@ -43,23 +43,23 @@ namespace SwiftFinancials.Web.Areas.Dashboard.Controllers
         //    return Json(new { success = true, AssetRegisters = _channelService.FindClaimsAsync(GetServiceHeader()).Result.Count });
         //}
 
-        //public async Task<JsonResult> FindMonthlyEmailAlertsStatistics()
-        //{
-        //    var monthsCap = Convert.ToInt32(ConfigurationManager.AppSettings["MessagingSummaryMonthCap"]) * -1;
+        public async Task<JsonResult> FindMonthlyEmailAlertsStatistics()
+        {
+            var monthsCap = Convert.ToInt32(ConfigurationManager.AppSettings["MessagingSummaryMonthCap"]) * -1;
 
-        //    var results = Json(await _channelService.FindEmailAlertsMonthlyStatisticsAsync(User.Identity.GetCompanyId(), DateTime.Today.AddMonths(monthsCap), DateTime.Now, GetServiceHeader()), JsonRequestBehavior.AllowGet);
+            var results = Json(await _channelService.FindEmailAlertsMonthlyStatisticsAsync(User.Identity.GetCompanyId(), DateTime.Today.AddMonths(monthsCap), DateTime.Now, GetServiceHeader()), JsonRequestBehavior.AllowGet);
 
-        //    return results;
-        //}
+            return results;
+        }
 
-        //public async Task<JsonResult> FindMonthlyTextAlertsStatistics()
-        //{
-        //    var monthsCap = Convert.ToInt32(ConfigurationManager.AppSettings["MessagingSummaryMonthCap"]) * -1;
+        public async Task<JsonResult> FindMonthlyTextAlertsStatistics()
+        {
+            var monthsCap = Convert.ToInt32(ConfigurationManager.AppSettings["MessagingSummaryMonthCap"]) * -1;
 
-        //    var results = Json(await _channelService.FindTextAlertsMonthlyStatisticsAsync(User.Identity.GetCompanyId(), DateTime.Today.AddMonths(monthsCap), DateTime.Now, GetServiceHeader()), JsonRequestBehavior.AllowGet);
+            var results = Json(await _channelService.FindTextAlertsMonthlyStatisticsAsync(User.Identity.GetCompanyId(), DateTime.Today.AddMonths(monthsCap), DateTime.Now, GetServiceHeader()), JsonRequestBehavior.AllowGet);
 
-        //    return results;
-        //}
+            return results;
+        }
 
         #endregion
 
