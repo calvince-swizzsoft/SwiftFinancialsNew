718116ee2edbde0ba9c77ae9447a607dc5e0dcf3
stamline <stamlinetechnologies@gmail.com>
2023-06-22 10:13:13 +0300

    Add Savings Product Ledger Fee Processing plugin

---
+++ SwiftFinancials.SavingsProductLedgerFeeProcessor/Configuration/SavingsProductLedgerFeeProcessingSettingsCollection.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+
+namespace SwiftFinancials.SavingsProductLedgerFeeProcessing.Configuration
+{
+    public class SavingsProductLedgerFeeProcessingSettingsCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new SavingsProductLedgerFeeProcessingSettingsElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((SavingsProductLedgerFeeProcessingSettingsElement)(element)).UniqueId;
+        }
+
+        public SavingsProductLedgerFeeProcessingSettingsElement this[int index]
+        {
+            get { return (SavingsProductLedgerFeeProcessingSettingsElement)BaseGet(index); }
+        }
+
+        [ConfigurationProperty("name", IsRequired = false)]
+        public string Name
+        {
+            get
+            {
+                return (string)base["name"];
+            }
+        }
+
+        [ConfigurationProperty("savingsProductLedgerFeeProcessingJobCronExpression", IsRequired = true)]
+        public string SavingsProductLedgerFeeProcessingJobCronExpression
+        {
+            get
+            {
+                return (string)base["savingsProductLedgerFeeProcessingJobCronExpression"];
+            }
+        }
+    }
+}
