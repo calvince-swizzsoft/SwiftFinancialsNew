b1b7b6d9b5f9164beea6d7ccd088718645a1dde0
BrianAhuga <142815137+BrianAhuga@users.noreply.github.com>
2023-09-02 13:05:51 +0300

    Database backUP

---
+++ SwiftFinancials.Web/Areas/Accounts/Controllers/GLAccountDeterminationController.cs
@@ -0,0 +1,29 @@
+﻿using SwiftFinancials.Web.Areas.Accounts.Models.GLAccountsDetermination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwiftFinancials.Web.Areas.Accounts.Controllers
+{
+    public class GLAccountDeterminationController : Controller
+    {
+        // GET: Accounts/GLAccountDetermination
+        public ActionResult GetDetails()
+        {
+            var data = new  GLAccountsDet();
+            
+            return View(data);
+        }
+
+        
+        public  GLADRply(GLAccountsDet glad)
+        {
+            string systemGLAccountCode2 = glad.systemGLAccountCode, mappedGLAccountName2 = glad.mappedGLAccountName,
+                mappedGLAccountcostCenter2 = glad.mappedGLAccountcostCenter, createdDate2 = glad.createdDate;
+
+           
+        }
+    }
+}
