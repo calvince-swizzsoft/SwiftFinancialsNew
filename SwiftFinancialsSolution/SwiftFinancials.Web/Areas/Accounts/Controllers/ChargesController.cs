74b7c3c08f083aca25e283f24c4d57eb3e5c5424
Brian Ahuga <142815137+BrianAhuga@users.noreply.github.com>
2024-04-16 04:19:56 +0300

    Charge

---
+++ SwiftFinancialsSolution/SwiftFinancials.Web/Areas/Accounts/Controllers/ChargesController.cs
@@ -0,0 +1,121 @@
+﻿using Application.MainBoundedContext.DTO;
+using Application.MainBoundedContext.DTO.AccountsModule;
+using SwiftFinancials.Web.Controllers;
+using SwiftFinancials.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SwiftFinancials.Web.Areas.Accounts.Controllers
+{
+    public class ChargesController : MasterController
+    {
+        public async Task<ActionResult> Index()
+        {
+            await ServeNavigationMenus();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Index(JQueryDataTablesModel jQueryDataTablesModel)
+        {
+            int totalRecordCount = 0;
+
+            int searchRecordCount = 0;
+
+            var sortAscending = jQueryDataTablesModel.sSortDir_.First() == "asc" ? true : false;
+
+            var sortedColumns = (from s in jQueryDataTablesModel.GetSortedColumns() select s.PropertyName).ToList();
+
+            var pageCollectionInfo = await _channelService.FindCommissionsByFilterInPageAsync(jQueryDataTablesModel.sSearch, jQueryDataTablesModel.iDisplayStart, jQueryDataTablesModel.iDisplayLength, GetServiceHeader());
+
+            if (pageCollectionInfo != null && pageCollectionInfo.PageCollection.Any())
+            {
+                totalRecordCount = pageCollectionInfo.ItemsCount;
+
+                pageCollectionInfo.PageCollection = pageCollectionInfo.PageCollection.OrderByDescending(costCenter => costCenter.CreatedDate).ToList();
+
+                searchRecordCount = !string.IsNullOrWhiteSpace(jQueryDataTablesModel.sSearch) ? pageCollectionInfo.PageCollection.Count : totalRecordCount;
+
+                return this.DataTablesJson(items: pageCollectionInfo.PageCollection, totalRecords: totalRecordCount, totalDisplayRecords: searchRecordCount, sEcho: jQueryDataTablesModel.sEcho);
+            }
+            else return this.DataTablesJson(items: new List<CommissionDTO> { }, totalRecords: totalRecordCount, totalDisplayRecords: searchRecordCount, sEcho: jQueryDataTablesModel.sEcho);
+        }
+
+        public async Task<ActionResult> Details(Guid id)
+        {
+            await ServeNavigationMenus();
+
+            var commissionDTO = await _channelService.FindCommissionAsync(id, GetServiceHeader());
+
+            return View(commissionDTO);
+        }
+        public async Task<ActionResult> Create()
+        {
+            await ServeNavigationMenus();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CommissionDTO commissionDTO)
+        {
+            commissionDTO.ValidateAll();
+
+            if (!commissionDTO.HasErrors)
+            {
+                await _channelService.AddCommissionAsync(commissionDTO, GetServiceHeader());
+
+                TempData["AlertMessage"] = "Charge created successfully";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var errorMessages = commissionDTO.ErrorMessages;
+
+                TempData["Error"] = "Failed to create Charge";
+
+                return View(commissionDTO);
+            }
+        }
+
+        public async Task<ActionResult> Edit(Guid id)
+        {
+            await ServeNavigationMenus();
+
+            var commissionDTO = await _channelService.FindCommissionAsync(id, GetServiceHeader());
+
+            return View(commissionDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Guid id, CommissionDTO commissionDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _channelService.UpdateCommissionAsync(commissionDTO, GetServiceHeader());
+
+                TempData["Edit"] = "Edited Charge successfully";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(commissionDTO);
+            }
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetCostCentersAsync()
+        {
+            var costCenterDTOs = await _channelService.FindCostCentersAsync(GetServiceHeader());
+
+            return Json(costCenterDTOs, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
