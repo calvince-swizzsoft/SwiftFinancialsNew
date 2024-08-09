d5f6f7108a400271ccea44013dba10b26dd16f4f
Nyansarora2023 <christineareba@swizzsoft.com>
2024-08-09 13:24:44 +0300

    kki

--- SwiftFinancialsSolution/Application.MainBoundedContext.DTO/HumanResourcesModule/DesignationDTO.cs
+++ SwiftFinancialsSolution/Application.MainBoundedContext.DTO/HumanResourcesModule/DesignationDTO.cs
@@ -13,6 +13,7 @@ namespace Application.MainBoundedContext.DTO.HumanResourcesModule
         public DesignationDTO()
         {
             AddAllAttributeValidators();
+            
         }
 
         [DataMember]
