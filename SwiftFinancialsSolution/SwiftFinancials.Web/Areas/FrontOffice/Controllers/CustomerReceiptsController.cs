99cdc86644236ae9ccc6930fbf0f001932ac43e9
calvince-swizzsoft <calvince.ochieng@swizzsoft.com>
2024-08-31 08:39:30 +0300

    + customerreceipts

--- SwiftFinancialsSolution/SwiftFinancials.Web/Areas/FrontOffice/Controllers/CustomerReceiptsController.cs
+++ SwiftFinancialsSolution/SwiftFinancials.Web/Areas/FrontOffice/Controllers/CustomerReceiptsController.cs
@@ -9,11 +9,18 @@ using Application.MainBoundedContext.DTO.AccountsModule;
 using Application.MainBoundedContext.DTO.FrontOfficeModule;
 using SwiftFinancials.Web.Controllers;
 using SwiftFinancials.Web.Helpers;
+using SwiftFinancials.Presentation.Infrastructure.Models;
+using Infrastructure.Crosscutting.Framework.Utils;
+using System.Net;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 
 namespace SwiftFinancials.Web.Areas.FrontOffice.Controllers
 {
     public class CustomerReceiptsController : MasterController
     {
+
+
         // GET: FrontOffice/CustomerReceipts
         public async Task<ActionResult> Index()
         {
@@ -23,125 +30,244 @@ namespace SwiftFinancials.Web.Areas.FrontOffice.Controllers
 
         }
 
-        public async Task<ActionResult> Create(Guid? id)
+
+        public async Task<ActionResult> Create(Guid? id, int? role)
         {
-
-
             await ServeNavigationMenus();
-         
             Guid parseId;
+
+
+
+            var model = new CustomerTransactionModel();
+
+            // Initialize both models
+            model.ApportionmentWrapper.CreditCustomerAccount = new CustomerAccountDTO();
+
+            //debit model
+            model.CustomerAccount = new CustomerAccountDTO();
 
             if (id == Guid.Empty || !Guid.TryParse(id.ToString(), out parseId))
             {
+                ViewBag.ApportionToSelectList = GetApportionToSelectList(string.Empty);
                 return View();
             }
 
+   
+            //string description = GetEnumDescription(transactionRole);
 
-            bool includeBalances = false;
-            bool includeProductDescription = false;
-            bool includeInterestBalanceForLoanAccounts = false;
-            bool considerMaturityPeriodForInvestmentAccounts = false;
+
+            bool includeBalances = true;
+            bool includeProductDescription = true;
+            bool includeInterestBalanceForLoanAccounts = true;
+            bool considerMaturityPeriodForInvestmentAccounts = true;
 
 
             var customer = await _channelService.FindCustomerAccountAsync(parseId, includeBalances, includeProductDescription, includeInterestBalanceForLoanAccounts, considerMaturityPeriodForInvestmentAccounts, GetServiceHeader());
 
-            CustomerAccountDTO customerAccountDTO = new CustomerAccountDTO();
 
-            if (customer != null)
+            if (role.HasValue)
             {
-                customerAccountDTO.Id = customer.Id;
-                customerAccountDTO.CustomerId = customer.CustomerId;
+                CustomerAccountTransactionRole transactionRole = (CustomerAccountTransactionRole)role;
+                switch (transactionRole)
+                {
 
-                customerAccountDTO.CustomerIndividualFirstName = customer.CustomerFullName;
-                customerAccountDTO.CustomerIndividualPayrollNumbers = customer.CustomerIndividualPayrollNumbers;
-                customerAccountDTO.CustomerSerialNumber = customer.CustomerSerialNumber;
-                customerAccountDTO.CustomerReference1 = customer.CustomerReference1;
-                customerAccountDTO.CustomerReference2 = customer.CustomerReference2;
-                customerAccountDTO.CustomerReference3 = customer.CustomerReference3;
-                customerAccountDTO.CustomerIndividualIdentityCardNumber = customer.CustomerIndividualIdentityCardNumber;
-                customerAccountDTO.Remarks = customer.Remarks;
-                customerAccountDTO.CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription;
-                customerAccountDTO.BranchId = customer.BranchId;
-                customerAccountDTO.BranchDescription = customer.BranchDescription;
-                customerAccountDTO.CustomerAccountTypeTargetProductId = customer.CustomerAccountTypeTargetProductId;
-                customerAccountDTO.CustomerAccountTypeTargetProductCode = customer.CustomerAccountTypeTargetProductCode;
-                customerAccountDTO.CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription;
-                customerAccountDTO.CustomerAccountTypeTargetProductParentId = customer.CustomerAccountTypeTargetProductParentId;
-                customerAccountDTO.AvailableBalance = customer.AvailableBalance;
-                customerAccountDTO.BookBalance = customer.BookBalance;
+                    case CustomerAccountTransactionRole.CreditCustomerAccount:
 
 
-            }
+                        if (Session["debitAcct"] != null)
+                        {
+                            model.CustomerAccount = Session["debitAcct"] as CustomerAccountDTO;
+                        }
+
+                        if (customer != null)
+                        {
+                            model.ApportionmentWrapper.FullAccountNumber = customer.FullAccountNumber;
+                            model.ApportionmentWrapper.CustomerFullName = customer.CustomerFullName;
+                            model.ApportionmentWrapper.CustomerReference1 = customer.CustomerReference1;
+                            model.ApportionmentWrapper.CreditCustomerAccount.Id = customer.Id;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerId = customer.CustomerId;
+
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerIndividualPayrollNumbers = customer.CustomerIndividualPayrollNumbers;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerSerialNumber = customer.CustomerSerialNumber;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerReference1 = customer.CustomerReference1;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerReference2 = customer.CustomerReference2;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerReference3 = customer.CustomerReference3;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerIndividualIdentityCardNumber = customer.CustomerIndividualIdentityCardNumber;
+                            model.ApportionmentWrapper.CreditCustomerAccount.Remarks = customer.Remarks;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription;
+                            model.ApportionmentWrapper.CreditCustomerAccount.BranchId = customer.BranchId;
+                            model.ApportionmentWrapper.CreditCustomerAccount.BranchDescription = customer.BranchDescription;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerAccountTypeTargetProductId = customer.CustomerAccountTypeTargetProductId;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerAccountTypeTargetProductCode = customer.CustomerAccountTypeTargetProductCode;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription;
+                            model.ApportionmentWrapper.CreditCustomerAccount.CustomerAccountTypeTargetProductParentId = customer.CustomerAccountTypeTargetProductParentId;
+                            model.ApportionmentWrapper.CreditCustomerAccount.AvailableBalance = customer.AvailableBalance;
+                            model.ApportionmentWrapper.CreditCustomerAccount.BookBalance = customer.BookBalance;
+                            model.ApportionmentWrapper.ProductDescription = customer.CustomerAccountTypeTargetProductDescription;
+                           
+                        }
+
+
+                        // After autofilling data
+
+                        Session["creditAcct"] = model.CreditCustomerAccount;
+
+
+
+                        break;
+
+                    case CustomerAccountTransactionRole.DebitCustomerAccount:
+
+                        if (Session["creditAcct"] != null)
+                        {
+                            model.CreditCustomerAccount = Session["creditAcct"] as CustomerAccountDTO;
+                        }
+
+
+                        //CustomerAccountDTO CustomerAccount = new CustomerAccountDTO();
+
+                        if (model != null)
+                        {
+
+
+                            model.CustomerAccount.Id = customer.Id;
+                            model.CustomerAccount.CustomerId = customer.CustomerId;
+
+                            model.CustomerAccount.CustomerIndividualFirstName = customer.CustomerFullName;
+                            model.CustomerAccount.CustomerIndividualPayrollNumbers = customer.CustomerIndividualPayrollNumbers;
+                            model.CustomerAccount.CustomerSerialNumber = customer.CustomerSerialNumber;
+                            model.CustomerAccount.CustomerReference1 = customer.CustomerReference1;
+                            model.CustomerAccount.CustomerReference2 = customer.CustomerReference2;
+                            model.CustomerAccount.CustomerReference3 = customer.CustomerReference3;
+                            model.CustomerAccount.CustomerIndividualIdentityCardNumber = customer.CustomerIndividualIdentityCardNumber;
+                            model.CustomerAccount.Remarks = customer.Remarks;
+                            model.CustomerAccount.CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription;
+                            model.CustomerAccount.BranchId = model.BranchId;
+                            model.CustomerAccount.BranchDescription = customer.BranchDescription;
+                            model.CustomerAccount.CustomerAccountTypeTargetProductId = customer.CustomerAccountTypeTargetProductId;
+                            model.CustomerAccount.CustomerAccountTypeTargetProductCode = customer.CustomerAccountTypeTargetProductCode;
+                            model.CustomerAccount.CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription;
+                            model.CustomerAccount.CustomerAccountTypeTargetProductParentId = customer.CustomerAccountTypeTargetProductParentId;
+                            model.CustomerAccount.AvailableBalance = customer.AvailableBalance;
+                            model.CustomerAccount.BookBalance = customer.BookBalance;
+
+                        }
+
+                        // After autofilling data
+
+                        Session["debitAcct"] = model.CustomerAccount;
+
+                        break;
+                   }
+
+                }
 
             ViewBag.WithdrawalNotificationCategorySelectList = GetWithdrawalNotificationCategorySelectList(string.Empty);
 
             ViewBag.TransactionTypeSelectList = GetFrontOfficeTransactionTypeSelectList(string.Empty);
 
-            return View(customerAccountDTO);
+            ViewBag.ApportionToSelectList = GetApportionToSelectList(string.Empty);
+
+            return View(model);
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Create(CustomerAccountDTO customerAccountDTO)
 
-
+        public async Task<JsonResult> GetCustomerDetailsJson(Guid? id, int? role)
         {
+            Guid parseId;
 
-
-            customerAccountDTO.ValidateAll();
-
-
-            if (!customerAccountDTO.HasErrors)
+            if (!Guid.TryParse(id.ToString(), out parseId))
             {
-                decimal totalValue = customerAccountDTO.TotalValue;
-                int frontOfficeTransactionType = customerAccountDTO.Type;
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
 
-                await _channelService.ComputeTellerCashTariffsAsync(customerAccountDTO, totalValue, frontOfficeTransactionType, GetServiceHeader());
+            bool includeBalances = true;
+            bool includeProductDescription = true;
+            bool includeInterestBalanceForLoanAccounts = true;
+            bool considerMaturityPeriodForInvestmentAccounts = true;
 
-                if (customerAccountDTO.TypeDescription.Equals("Cash Withdrawal"))
-                {
+            var customer = await _channelService.FindCustomerAccountAsync(parseId, includeBalances, includeProductDescription, includeInterestBalanceForLoanAccounts, considerMaturityPeriodForInvestmentAccounts, GetServiceHeader());
 
-                    CashWithdrawalRequestDTO cashWithdrawalRequestDTO = new CashWithdrawalRequestDTO();
+            if (customer == null)
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
 
-                    cashWithdrawalRequestDTO.ValidateAll();
+            var model = new
+            {
+               FullAccountNumber = customer.FullAccountNumber,
+                CustomerFullName = customer.CustomerFullName,
+                CustomerReference1 = customer.CustomerReference1,
+                CustomerReference2 = customer.CustomerReference2,
+                BranchDescription = customer.BranchDescription,
+                AvailableBalance =  customer.AvailableBalance,
+                BranchId = customer.BranchId,
+                CustomerAccountTypeTargetProductId = customer.CustomerAccountTypeTargetProductId,
+                CustomerAccountTypeTargetProductCode = customer.CustomerAccountTypeTargetProductCode,
+                CustomerAccountTypeTargetProductDescription = customer.CustomerAccountTypeTargetProductDescription,
+                CustomerAccountTypeTargetProductParentId = customer.CustomerAccountTypeTargetProductParentId,
+                Id = customer.Id,
+                CustomerId = customer.CustomerId
 
-                    await _channelService.AddCashWithdrawalRequestAsync(cashWithdrawalRequestDTO, GetServiceHeader());
+        };
 
-                }
-
-                else if (customerAccountDTO.TypeDescription.Equals("Cash Deposit"))
-                {
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
 
 
-                    CashDepositRequestDTO cashDepositRequestDTO = new CashDepositRequestDTO();
-
-                    cashDepositRequestDTO.ValidateAll();
-
-                    await _channelService.AddCashDepositRequestAsync(cashDepositRequestDTO, GetServiceHeader());
-
-                }
-
-                else if (customerAccountDTO.TypeDescription.Equals("Cheque Deposit"))
-                {
 
 
-                    ExternalChequeDTO chequeDepositDTO = new ExternalChequeDTO();
-                    chequeDepositDTO.ValidateAll();
 
-                    await _channelService.AddExternalChequeAsync(chequeDepositDTO, GetServiceHeader());
 
-                }
+        [HttpPost]
+        public async Task<ActionResult> Create(CustomerTransactionModel model)
+        {
+            model.CustomerAccount.ValidateAll();
+            var rawApportionments = model.Apportionments;
+            // Deserialize the ApportionmentsData JSON field
+            //var apportionmentsJson = Request.Form["ApportionmentsData"];
+            //var apportionments = JsonConvert.DeserializeObject<List<ApportionmentWrapper>>(apportionmentsJson);
 
-                ViewBag.TransactionTypeSelectList = GetFrontOfficeTransactionTypeSelectList(customerAccountDTO.CustomerAccountManagementActionDescription.ToString());
+            // Add the deserialized apportionments to your model
+            //model.Apportionments = apportionments;
 
+
+             if (!model.HasErrors)
+            {
+                var currentPostingPeriod = await _channelService.FindCurrentPostingPeriodAsync(GetServiceHeader());
+                
+                decimal totalValue = model.CustomerAccount.TotalValue;
+                
+                int apportionTo = model.CustomerAccount.Type;
+
+                var transactionModel = new TransactionModel();
+
+                transactionModel.TotalValue = model.TotalValue;
+                transactionModel.DebitCustomerAccount = model.CustomerAccount;
+                transactionModel.DebitChartOfAccountId = model.CustomerAccount.CustomerAccountTypeTargetProductChartOfAccountId;
+
+                transactionModel.PostingPeriodId = currentPostingPeriod.Id;
+
+                var tariffs = await _channelService.ComputeTellerCashTariffsAsync(model.CustomerAccount, totalValue, 2, GetServiceHeader());
+                var dynamicCharges = await _channelService.FindDynamicChargesAsync(GetServiceHeader());
+
+                
+                ObservableCollection<ApportionmentWrapper> apportionmentsObservableCollection = new ObservableCollection<ApportionmentWrapper>(model.Apportionments);
+
+
+                await _channelService.AddJournalWithApportionmentsAsync(transactionModel, apportionmentsObservableCollection, tariffs, dynamicCharges, GetServiceHeader());
+
+                ViewBag.ApportionToSelectList = GetApportionToSelectList(string.Empty);
+                
                 return RedirectToAction("Create");
             }
             else
             {
-                var errorMessages = customerAccountDTO.ErrorMessages;
-                // ViewBag.TellerTypeSelectList = GetTellerTypeSelectList(cashDepositRequestDTO.CustomerAccountCustomerTypeDescription.ToString());
-                ViewBag.TransactionTypeSelectList = GetFrontOfficeTransactionTypeSelectList(customerAccountDTO.Type.ToString());
-
-                return View(customerAccountDTO);
+                var errorMessages = model.ErrorMessages;
+    
+                ViewBag.ApportionToSelectList = GetApportionToSelectList(string.Empty);
+                
+                return View(model);
             }
         }
     }
