08e27e21060b57dec33d9deb62d4126305659c63
Brian Ahuga <142815137+BrianAhuga@users.noreply.github.com>
2024-09-11 10:33:16 +0300

    Customers Specimen Capture

---
+++ SwiftFinancialsSolution/SwiftFinancials.Web/Areas/Registry/DocumentsModel/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SwiftFinancials.Web.Areas.Registry.DocumentsModel
+{
+    public class Document
+    {
+        public Guid Id { get; set; }  // Use Guid for unique identifier
+        public Guid CustomerId { get; set; }  // Use Guid for unique identifier
+        public byte[] PassportPhoto { get; set; }
+        public byte[] SignaturePhoto { get; set; }
+        public byte[] IDCardFrontPhoto { get; set; }
+        public byte[] IDCardBackPhoto { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
